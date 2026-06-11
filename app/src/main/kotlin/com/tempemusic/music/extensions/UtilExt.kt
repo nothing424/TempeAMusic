469d57cdc9409329bbb5dc318ca9865538d6dd7e
nothing424 <tzerothcoret@gmail.com>
2026-06-11 22:54:21 +0700

    Upload: app/src/main/kotlin/com/tempemusic/music/extensions/UtilExt.kt

---
+++ app/src/main/kotlin/com/tempemusic/music/extensions/UtilExt.kt
@@ -0,0 +1,13 @@
+/**
+ * TempeMusic Project (C) 2026
+ * Licensed under GPL-3.0 | See git history for contributors
+ */
+
+package com.tempemusic.music.extensions
+
+fun <T> tryOrNull(block: () -> T): T? =
+    try {
+        block()
+    } catch (e: Exception) {
+        null
+    }
